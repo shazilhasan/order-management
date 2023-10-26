7a1371433812f69c721e7af615e46cabf52f8a28
shazilhasan <146967418+shazilhasan@users.noreply.github.com>
2023-10-26 18:21:33 +0530

    rest.js

--- routes/rest.js
+++ routes/rest.js
@@ -1,61 +1,83 @@
-var express = require('express');
-var _ = require('lodash');
-var router = express.Router();
-var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/orders');
-mongoose.connect('mongodb://portaltechnologics-server:frHFtTzsDnXiePXeRp6lNL4f3omNUpUpuk0zKom08myfeLiRJQIxxJEuad0eRagX9nThg0sB1tIxACDbME0weA==@portaltechnologics-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@portaltechnologics-server@');
-mongoose.Promise = global.Promise;
+const express = require('express');
+const mongoose = require('mongoose');
+const CosmosClient = require('@azure/cosmos').CosmosClient;
 
-var Order = mongoose.model('Order',
-    {
-        name: String,
-        type: String,
-        size: String,
-        price: Number,
-        quantity: Number,
-        amount: Number,
-        time: Date
-    });
-var Drink = mongoose.model('Drink',
-    {
-        type: String,
-        name: String,
-        size: String,
-        price: Number
-    });
+const app = express();
+app.use(express.json());
 
-router.get('/drink', function (req, res, next) {
-    Drink.find(function (err, drinks) {
-        if (err) {
-            console.log(err);
-            res.status(500).send({success: false, message: err});
-        } else {
-            res.send(drinks);
-        }
-    });
+// MongoDB connection
+mongoose.connect('mongodb://portaltechnologics-server:frHFtTzsDnXiePXeRp6lNL4f3omNUpUpuk0zKom08myfeLiRJQIxxJEuad0eRagX9nThg0sB1tIxACDbME0weA==@portaltechnologics-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@portaltechnologics-server@', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-router.get('/order', function (req, res, next) {
-    Order.find(function (err, orders) {
-        if (err) {
-            console.log(err);
-            res.send({success: false, message: err});
-        } else {
-            res.send(orders);
-        }
-    });
+const Order = mongoose.model('Order', {
+  name: String,
+  type: String,
+  size: String,
+  price: Number,
+  quantity: Number,
+  amount: Number,
+  time: Date,
 });
 
-router.put('/order', function (req, res, next) {
-    var newOrder = new Order(req.body);
-    newOrder.time = new Date();
-    newOrder.save(function (err) {
-        if (err) {
-            console.log(err);
-            res.status(500).send({success: false, message: err});
-        } else {
-            res.send({success: true});
-        }
-    });
+// Azure Cosmos DB connection
+const cosmosClient = new CosmosClient({
+  endpoint: 'https://portaltechnologics-server.documents.azure.com:443/',
+  key: 'frHFtTzsDnXiePXeRp6lNL4f3omNUpUpuk0zKom08myfeLiRJQIxxJEuad0eRagX9nThg0sB1tIxACDbME0weA==',
 });
-module.exports = router;
+
+const databaseId = 'portaltechnologics-server';
+const containerId = '';
+const container = cosmosClient.database(databaseId).container(containerId);
+
+// MongoDB routes
+app.get('/mongo/orders', async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.json(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Error fetching MongoDB orders' });
+  }
+});
+
+app.post('/mongo/orders', async (req, res) => {
+  const newOrder = new Order(req.body);
+  newOrder.time = new Date();
+  try {
+    await newOrder.save();
+    res.json({ success: true, message: 'MongoDB order created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Error creating MongoDB order' });
+  }
+});
+
+// Cosmos DB routes
+app.get('/cosmos/orders', async (req, res) => {
+  try {
+    const { resources: cosmosOrders } = await container.items.readAll().fetchAll();
+    res.json(cosmosOrders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Error fetching Cosmos DB orders' });
+  }
+});
+
+app.post('/cosmos/orders', async (req, res) => {
+  const newItem = req.body;
+  try {
+    const { resource: cosmosOrder } = await container.items.create(newItem);
+    res.json({ success: true, data: cosmosOrder });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Error creating Cosmos DB order' });
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
